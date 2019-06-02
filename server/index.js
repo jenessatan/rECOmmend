9522ea931f54e702f58a54b9b3bf59a499b47b14
jenessa <jenessaxtan@gmail.com>
2019-06-01 21:36:42 -0700

    fix typo, update eslint config

--- server/index.js
+++ server/index.js
@@ -4,17 +4,27 @@ const bodyParser = require('body-parser');
 
 require('dotenv').config();
 
-var db = require('./database');
-var api = require('./api');
+// const db = require('./database');
+const api = require('./api');
 
 const ENV = process.env.NODE_ENV;
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
-app.use(bodyParser.join());
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-// app.use('/api/[]', require('./api/[]))
+app.get('/', (req, res) => {
+  res.send('This is rECOmmend!');
+});
+
+app.listen(PORT, () => {
+  if (ENV === 'development') {
+    console.log(`DEV: App is running and listening on Port ${PORT}`);
+  } else {
+    console.log('App is running');
+  }
+});
 
 module.exports = app;

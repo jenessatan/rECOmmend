92c01072a991687288968243cd58ddad3cc180e9
Eris <erislamw@alumni.ubc.ca>
2019-06-06 13:14:05 -0700

    Installed CORS

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 require('dotenv').config();
 
@@ -11,6 +12,7 @@ const ENV = process.env.NODE_ENV;
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());

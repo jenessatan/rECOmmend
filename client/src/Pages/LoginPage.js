170669bf14896490dfe93c69bce06e4b0a456b64
jenessa <jenessaxtan@gmail.com>
2019-06-06 15:31:27 -0700

    complete login interface for consumer and business

--- client/src/Pages/LoginPage.js
+++ client/src/Pages/LoginPage.js
@@ -1,10 +1,16 @@
 import React from 'react'
-import Login from '../Components/Login/Login.js'
+import Login from '../Components/Login/Login.js';
+import {Container} from 'reactstrap';
+
 import '../App.scss';
 
 function LoginPage() {
   return (
-    <Login />
+    <Container fluid className='loginPage'>
+        <div class='row justify-content-md-center align-items-center vh-100'>
+          <Login />
+        </div>
+    </Container>
   );
 }
 

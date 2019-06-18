1adf09a0160d4ce418843832523fff78cc93508b
karinawong <karinaytwong@gmail.com>
2019-06-18 13:54:13 -0700

    Card image WIP

--- client/src/Components/Cards/ProductItem.js
+++ client/src/Components/Cards/ProductItem.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
-import { Button, Card, CardImg, CardBody, CardTitle, CardSubtitle, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
-import elephantlogo from '../../Assets/elephantlogo.png';
+import { Button, Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import elephantlogo from '../../Assets/products/elephantlogo.png';
 import prodid1 from '../../Assets/products/prodid1.jpg';
 import prodid2 from '../../Assets/products/prodid2.jpg';
 import prodid3 from '../../Assets/products/prodid3.jpg';
@@ -17,6 +17,11 @@ class ProductItem extends Component {
     }));
   };
 
+  // componentWillReceiveProps(props) {
+  //   console.log('willreceive');
+  //   this.setState({image: this.props.image});
+  // }
+
   render() {
     return (
       <div>
@@ -30,8 +35,9 @@ class ProductItem extends Component {
           </ModalFooter>
         </Modal>
       <Card>
-        <CardImg top width="100%" src={this.props.image} alt="Card image" />
+        <img width="100%" src={this.props.image} alt="Card image" />
         <CardBody height="">
+          <CardText>{this.props.image}</CardText>
           <CardTitle className='cardPostTitle productTitle'>{this.props.title}  {'- $'+ this.props.price} </CardTitle>
           <CardSubtitle className='cardPostSubtitle productDescription'>{this.props.description}</CardSubtitle>
           <Button color="link" onClick={this.toggleModal}>See Retailers</Button>

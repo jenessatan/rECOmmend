5a3871449073a6360887d3372a630474b49ffa62
jenessatan <jenessaxtan@gmail.com>
2020-06-10 16:06:15 -0700

    change db setup for heroku db fix ... hopefully

--- server/db/config.js
+++ server/db/config.js
@@ -9,25 +9,26 @@ const options = {
 const pgp = require('pg-promise')(options);
 
 const setDB = () => {
-  if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) {
-    return pgp({
-      database: process.env.PGDB,
-      port: process.env.PGPORT,
-      host: process.env.PGHOST,
-      user: process.env.PGUSER,
-      password: process.env.PGPASSWORD,
-      ssl: true
-    });
-  } if (process.env.NODE_ENV === 'production') {
-    return pgp({
-      database: process.env.PGDB,
-      port: process.env.PGPORT,
-      host: process.env.PGHOST,
-      user: process.env.PGUSER,
-      password: process.env.PGPASSWORD,
-      ssl: true
-    });
-  }
+  return pgp(process.env.DATABASE_URL)
+  // if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) {
+    // return pgp({
+    //   database: process.env.PGDB,
+    //   port: process.env.PGPORT,
+    //   host: process.env.PGHOST,
+    //   user: process.env.PGUSER,
+    //   password: process.env.PGPASSWORD,
+    //   ssl: true
+    // });
+  // } if (process.env.NODE_ENV === 'production') {
+  //   return pgp({
+  //     database: process.env.PGDB,
+  //     port: process.env.PGPORT,
+  //     host: process.env.PGHOST,
+  //     user: process.env.PGUSER,
+  //     password: process.env.PGPASSWORD,
+  //     ssl: true
+  //   });
+  // }
 };
 
 const db = setDB();
